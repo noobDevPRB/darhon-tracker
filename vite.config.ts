f1f2682f06c5b670e8dc1eea37aed038d7ee4282
Bruno Pereira Dev <48847856+noobDevPRB@users.noreply.github.com>
2025-06-01 14:19:30 -0300

    vite.config.ts moved to root

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,6 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: '/darhon-tracker/', // <-- ESSENCIAL
+  base: '/darhon-tracker/',
   plugins: [react()],
 })
